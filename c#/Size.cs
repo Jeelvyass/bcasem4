9bd08a55eb1031a65a1e4bf2ec14598a04b8936c
Jeelvyass <zeelvyas1012@gmail.com>
2025-01-30 09:27:42 +0530

    Add files via upload

---
+++ c#/Size.cs
@@ -0,0 +1,14 @@
+using System;
+class Size
+{
+	public static void Main()
+	{
+		Console.WriteLine("sizeof(int): "+ sizeof(int));
+		Console.WriteLine("sizeof(float): "+ sizeof(float));
+		Console.WriteLine("sizeof(char): "+ sizeof(char));
+		Console.WriteLine("sizeof(double): "+ sizeof(double));
+		Console.WriteLine("sizeof(short): "+ sizeof(short));
+		Console.WriteLine("sizeof(long): "+ sizeof(long));
+		Console.WriteLine("sizeof(decimal): "+ sizeof(decimal));
+	}
+}
