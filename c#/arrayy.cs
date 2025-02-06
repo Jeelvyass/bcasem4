f9cfb764c800a22596634fcd9d9e5c18604b48eb
Jeelvyass <zeelvyas1012@gmail.com>
2025-02-05 19:50:59 -0800

    Add files via upload

---
+++ c#/arrayy.cs
@@ -0,0 +1,12 @@
+using System;
+class arrayy
+{
+	public static void Main()
+	{
+			Console.WriteLine("enter your message: ");
+			int n;
+			n=Convert.ToInt32(Console.ReadLine());
+			int[] arr=new int[n];
+			Console.WriteLine("you entered: " + n);
+	}
+}
