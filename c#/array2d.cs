ad4f4bd2784da06a62d45c75912a68279c97aa56
Jeelvyass <zeelvyas1012@gmail.com>
2025-02-11 21:59:18 -0800

    Update array2d.cs

--- c#/array2d.cs
+++ c#/array2d.cs
@@ -8,9 +8,8 @@ class array2d
 		int[,] values = new int[s,f];
 		Console.WriteLine("s:" +s);
 		Console.WriteLine("f:" +f);
-			Console.WriteLine(values.length);
-		
-		/*
+	 		
+
 		for (int i = 0; i < values.GetLength(0); i++) 
 		{ 
 			for (int j = 0; j < values.GetLength(1); j++) 
@@ -19,14 +18,14 @@ class array2d
 			} 
 		} 
 		
-			Console.WriteLine("you enterd a value is: \n"); 
+		Console.WriteLine("you enterd a value is: \n"); 
 		for (int i = 0; i < values.GetLength(0); i++) 
 		{ 
 			for (int j = 0; j < values.GetLength(1); j++) 
 			{ 
 			
-				Console.WriteLine("index {0}{1}: ",i,j,values[i, j]); 
+				Console.WriteLine("index {0} {1}:  {2} ",i,j,values[i, j]); 
 			} 
-		} */
+		} 
 	}
 }
