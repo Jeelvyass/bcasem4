f5fe61f7cd35d4d34fc466369c427ebd5dbe0eda
Jeelvyass <zeelvyas1012@gmail.com>
2025-02-18 22:36:55 +0530

    Add files via upload

---
+++ c#/prac_6.cs
@@ -0,0 +1,50 @@
+using System;
+
+class Array2D
+{
+    public static void Main()
+    {
+        // Get the number of rows and columns from the user
+        int s = Convert.ToInt32(Console.ReadLine()); // number of rows
+        int f = Convert.ToInt32(Console.ReadLine()); // number of columns
+        
+        // Declare a 2D array with the user-specified size
+        int[,] values = new int[s, f];
+
+        Console.WriteLine("s: " + s);
+        Console.WriteLine("f: " + f);
+
+        // Fill the array with user input
+        for (int i = 0; i < values.GetLength(0); i++)
+        {
+            for (int j = 0; j < values.GetLength(1); j++)
+            {
+                //Console.Write("Enter value for index [{i},{j}]: ");
+                Console.Write("Enter value for index [{0},{1}]: ",i,j);
+                values[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // Display the entered values
+        Console.WriteLine("\nYou entered the following values:");
+        for (int i = 0; i < values.GetLength(0); i++)
+        {
+            for (int j = 0; j < values.GetLength(1); j++)
+            {
+                Console.WriteLine("Index [{0},{1}]: {2}",i,j,values[i,j]);
+            }
+        }
+
+        // Sum of each row
+        Console.WriteLine("\nSum of each row:");
+        for (int i = 0; i < s; i++)
+        {
+            int rowSum = 0;
+            for (int j = 0; j < f; j++)
+            {
+                rowSum += values[i, j];
+            }
+            Console.WriteLine("Sum of row {0}: {1}",i+1,rowSum);
+        }
+    }
+}
