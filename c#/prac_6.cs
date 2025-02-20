445c11df54119b51d8b34e357992407f0f8fb8e0
Jeelvyass <zeelvyas1012@gmail.com>
2025-02-19 22:03:16 -0800

    Update prac_6.cs

--- c#/prac_6.cs
+++ c#/prac_6.cs
@@ -4,29 +4,27 @@ class Array2D
 {
     public static void Main()
     {
-        // Get the number of rows and columns from the user
+        // Get rows and columns from the user
         int s = Convert.ToInt32(Console.ReadLine()); // number of rows
         int f = Convert.ToInt32(Console.ReadLine()); // number of columns
         
-        // Declare a 2D array with the user-specified size
         int[,] values = new int[s, f];
 
         Console.WriteLine("s: " + s);
         Console.WriteLine("f: " + f);
 
-        // Fill the array with user input
+        // get value from user
         for (int i = 0; i < values.GetLength(0); i++)
         {
             for (int j = 0; j < values.GetLength(1); j++)
             {
-                //Console.Write("Enter value for index [{i},{j}]: ");
                 Console.Write("Enter value for index [{0},{1}]: ",i,j);
                 values[i, j] = Convert.ToInt32(Console.ReadLine());
             }
         }
 
         // Display the entered values
-        Console.WriteLine("\nYou entered the following values:");
+        Console.WriteLine("\nYou entered the values:");
         for (int i = 0; i < values.GetLength(0); i++)
         {
             for (int j = 0; j < values.GetLength(1); j++)
