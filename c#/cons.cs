95962896081a36d260000edd5beea92a85f0aad2
Jeelvyass <zeelvyas1012@gmail.com>
2025-02-24 22:53:03 -0800

    Update cons.cs

--- c#/cons.cs
+++ c#/cons.cs
@@ -1,28 +1,25 @@
 using System;
 class cons
 {
-    int a;
-    int b;
-    public cons()
+    static int a;
+    static int b,ans;
+	public cons(){}
+    public cons(int a1,int b1)
     {
-		a=0;
-		b=0;
-	}
-    public cons(int a,int b)
-    {
-        a=20;
-        b=30;
+        a=a1;
+        b=b1;
     } 
-    public  int sum()
+    public  void sum()
     {
-        return a+b;
+        ans= a+b;
+	   
     }
     public static void Main()
     {
-        cons o1=new cons();
-        cons o2=new cons(o1.a,o1.b);
-        cons o3=new cons();
-       int ans= o3.sum();
-        Console.WriteLine("sum is: "+ans);
+       //cons o1=new cons();
+        cons o2=new cons(10,20);
+		o2.sum();
+		Console.WriteLine("sum is: "+ans);
+        
     }
 }
