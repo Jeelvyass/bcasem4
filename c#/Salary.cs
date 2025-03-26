237d22052c75952ced4cea5c00cec71ff7026a6e
Jeelvyass <zeelvyas1012@gmail.com>
2025-03-25 21:47:36 -0700

    Update Salary.cs

--- c#/Salary.cs
+++ c#/Salary.cs
@@ -1,14 +1,13 @@
 using System;
-
 class Salary
-{
-    // Method to calculate Gross Salary using ref and out parameters
-    public void Calculation(ref int basicsalary, out double grosssalary)
+{ 
+    public void Calculation(ref int basicsalary, out double grosssalary,out double TA,out double DA, out double HRA, out double PF)
     {
-        // Declare and initialize variables
-        double TA = 0, DA = 0, HRA = 0, PF = 0;
+         TA = 0;
+		 DA = 0;
+		 HRA = 0;
+		 PF = 0;
 
-        // Apply salary conditions
         if (basicsalary >= 35000 && basicsalary <= 45000)
         {
             Console.WriteLine("\nYour basic salary is between 35000 and 45000.");
@@ -16,6 +15,8 @@ class Salary
             DA = (basicsalary * 5.0) / 100;  // 5%
             HRA = (basicsalary * 15.0) / 100; // 15%
             PF = (basicsalary * 8.5) / 100;  // 8.5%
+			
+           
         }
         else if (basicsalary < 35000)
         {
@@ -24,9 +25,9 @@ class Salary
             DA = (basicsalary * 12.0) / 100; // 12%
             HRA = (basicsalary * 20.0) / 100; // 20%
             PF = (basicsalary * 8.5) / 100;  // 8.5%
+			
         }
 
-        // Calculate Gross Salary
         grosssalary = basicsalary + TA + DA + HRA - PF;
     }
 }
@@ -37,16 +38,18 @@ class Program
     {
         Console.Write("Enter Basic Salary: ");
         int basicsalary = Convert.ToInt32(Console.ReadLine()); 
+		 double TA, DA , HRA , PF ;
         double grosssalary; // Out variable
 
-        // Create object of Salary class
         Salary obj = new Salary();
 
-        // Call Calculation method
-        obj.Calculation(ref basicsalary, out grosssalary);
-
-        // Print Gross Salary
+        obj.Calculation(ref basicsalary, out grosssalary,out TA,out DA, out HRA, out PF);
         Console.WriteLine("\nYou entered Basic Salary: " + basicsalary);
+		
+			Console.WriteLine("TA: "+TA);
+			Console.WriteLine("DA: "+DA);
+			Console.WriteLine("HRA: "+HRA);
+			Console.WriteLine("PF: "+PF);
         Console.WriteLine("Your Gross Salary is: " + grosssalary);
     }
 }
